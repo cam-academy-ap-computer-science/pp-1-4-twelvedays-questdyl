74f8b35cced3baf61e8c4d5bbfccc35d60b22c37
questdyl000 <questdyl000@CAM-LAB-23.bgsd.k12.wa.us>
2019-09-17 07:40:02 -0700

    Done

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,8 +2,93 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// "The Twelve Days of Christmas"
+		System.out.println("\nOn the 1 day of Christmas,");
+		truelove();
+		part();
+		System.out.println("\nOn the 2 day of Christmas,");
+		truelove();
+		dove();
+		System.out.println("\nOn the 3 day of Christmas,");
+		truelove();
+		hens();
+		System.out.println("\nOn the 4 day of Christmas,");
+		truelove();
+		birds()
+		System.out.println("\nOn the 5 day of Christmas,");
+		truelove();
+		rings();
+		System.out.println("\nOn the 6 day of Christmas,");
+		truelove();
+		geese();
+		System.out.println("\nOn the 7 day of Christmas,");
+		truelove();
+		swans();
+		System.out.println("\nOn the 8 day of Christmas,");
+		truelove();
+		maids();
+		System.out.println("\nOn the 9 day of Christmas,");
+		truelove();
+		ladies();
+		System.out.println("\nOn the 10 day of Christmas,");
+		truelove();
+		lords();
+		System.out.println("\nOn the 11 day of Christmas,");
+		truelove();
+		pipers();
+		System.out.println("\nOn the 12 day of Christmas,");
+		truelove();
+		drum();
+	}
+	public static void truelove() {
+		System.out.println("my true love sent to me")
+	}
+	public static void part() {
+		System.out.println("a partridge in a pear tree.");
+	}
+	public static void dove() {
+		System.out.println("two turtle doves, and");
+		part();
+	}
+	public static void hens() {
+		System.out.println("three French hens,");
+		dove();
+	}
+	public static void birds() {
+		System.out.println("four calling birds,");
+		hens()
+	}
+	public static void rings() {
+		System.out.println("five golden rings,");
+		birds();
+	}
+	public static void geese() {
+		System.out.println("six gees a-laying,");
+		rings();
+	}
+	public static void swans() {
+		System.out.println("seven swans a-swimming,");
+		geese();
+	}
+	public static void maids() {
+		System.out.println("eight maids a-milking,");
+		swans();
+	}
+	public static void ladies() {
+		System.out.println("nine ladies dancing,");
+		maids();
+	}
+	public static void lords() {
+		System.out.println("ten lords a-leaping,");
+		ladies();
+	}
+	public static void pipers() {
+		System.out.println("eleven pipers piping,");
+		lords();
+	}
+	public static void drum() {
+		System.out.println("Twelve drummers drumming,");
+		pipers()
 	}
 
 }
